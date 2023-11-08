cd20af8f405bf847e60dd747c67cb0299d9d1863
claudiomelendres <claudiomelendres@gmail.com>
2023-11-08 14:07:21 -0400

    Add project files.

---
+++ u01-signalRSample/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace u01_signalRSample.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
